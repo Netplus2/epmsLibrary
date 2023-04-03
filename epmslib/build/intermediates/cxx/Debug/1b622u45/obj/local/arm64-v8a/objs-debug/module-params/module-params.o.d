63f03e80d371bd2a1a99776150c38979bd0839fc
dsofttech <dynamicsofttechnology@gmail.com>
2023-04-03 04:48:30 +0100

    successfully ran the project, creating first release

--- epmslib/build/intermediates/cxx/Debug/1b622u45/obj/local/arm64-v8a/objs-debug/module-params/module-params.o.d
+++
@@ -1,2 +0,0 @@
-/Users/adebayooloyede/Desktop/SAdebayoAndroid/EpmsLibrary/epmslib/build/intermediates/cxx/Debug/1b622u45/obj/local/arm64-v8a/objs-debug/module-params/module-params.o: \
-  /Users/adebayooloyede/Desktop/SAdebayoAndroid/EpmsLibrary/epmslib/src/main/jni/module-params.c
--- epmslib/build/intermediates/cxx/Debug/1b622u45/obj/local/x86_64/objs-debug/module-params/module-params.o.d
+++
@@ -1,2 +0,0 @@
-/Users/adebayooloyede/Desktop/SAdebayoAndroid/EpmsLibrary/epmslib/build/intermediates/cxx/Debug/1b622u45/obj/local/x86_64/objs-debug/module-params/module-params.o: \
-  /Users/adebayooloyede/Desktop/SAdebayoAndroid/EpmsLibrary/epmslib/src/main/jni/module-params.c
